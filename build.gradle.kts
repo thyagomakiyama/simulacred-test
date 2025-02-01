8e1afaedc9aa2bdff34b972be66f84086e530e72
Thyago Makiyama <thyago.makiyama@gmail.com>
2025-02-01 14:10:33 -0300

    feat: add kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    id("io.kotest") version "0.4.11"
 }
 
 allprojects {
@@ -18,18 +19,21 @@ allprojects {
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.kotest")
 
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         testImplementation("org.jetbrains.kotlin:kotlin-test")
+        testImplementation("io.kotest:kotest-runner-junit5-jvm:5.9.1")
+        testImplementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
     }
 
     extensions.configure<JavaPluginExtension> {
         toolchain.languageVersion.set(JavaLanguageVersion.of(21))
     }
 
-    tasks.withType<Test> {
+    tasks.withType<Test>().configureEach {
         useJUnitPlatform()
     }
 }
