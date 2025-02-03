58266c1aeebd7f7cc00a3e76a437f6f9ad956e52
Thyago Makiyama <thyago.makiyama@gmail.com>
2025-02-03 18:38:32 -0300

    feat: implements simulation in memory repository

--- api/src/main/kotlin/com/simulacred/configuration/usecases/SimulationUseCaseConfig.kt
+++ api/src/main/kotlin/com/simulacred/configuration/usecases/SimulationUseCaseConfig.kt
@@ -2,13 +2,15 @@ package com.simulacred.configuration.usecases
 
 import com.simulacred.domain.ports.Logger
 import com.simulacred.domain.usecases.SimulationUseCase
+import com.simulacred.persistence.SimulationInMemoryRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SimulationUseCaseConfig(
     private val logger: Logger,
+    private val simulationInMemoryRepository: SimulationInMemoryRepository
 ) {
     @Bean
-    fun simulationUseCase(): SimulationUseCase = SimulationUseCase(logger)
+    fun simulationUseCase(): SimulationUseCase = SimulationUseCase(logger, simulationInMemoryRepository)
 }
---
+++ api/src/main/kotlin/com/simulacred/persistence/SimulationInMemoryRepository.kt
@@ -0,0 +1,26 @@
+package com.simulacred.persistence
+
+import com.simulacred.domain.ports.SimulationRepository
+import com.simulacred.domain.simulation.Simulation
+import org.springframework.stereotype.Component
+
+@Component
+class SimulationInMemoryRepository : SimulationRepository{
+    private val simulations = mutableListOf<Simulation>()
+
+    override suspend fun store(simulation: Simulation) {
+        simulations.add(simulation)
+    }
+
+    override suspend fun getById(id: String): Simulation? {
+        return simulations.find { it.id == id }
+    }
+
+    override suspend fun getByBorrowerEmail(email: String): List<Simulation> {
+        return simulations.filter { it.borrowerEmail == email }
+    }
+
+    override suspend fun getAll(): List<Simulation> {
+        return simulations
+    }
+}
